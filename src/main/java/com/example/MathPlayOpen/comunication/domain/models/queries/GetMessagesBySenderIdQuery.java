c6f138370bd57927ad80370a87c1a2151898df9e
MiguelC <sackito992@gmail.com>
2024-06-24 20:22:30 -0500

    feat: delete bc that are not relevant in this branch

--- src/main/java/com/example/MathPlayOpen/comunication/domain/models/queries/GetMessagesBySenderIdQuery.java
+++
@@ -1,9 +0,0 @@
-package com.example.MathPlayOpen.comunication.domain.models.queries;
-
-public record GetMessagesBySenderIdQuery(Long senderId) {
-    public GetMessagesBySenderIdQuery {
-        if (senderId == null) {
-            throw new IllegalArgumentException("senderId cannot be null");
-        }
-    }
-}
