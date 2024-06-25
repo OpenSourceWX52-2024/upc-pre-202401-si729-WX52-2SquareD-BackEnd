7d105774e3f4dfb25e38e34af41fdfc036712bc2
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-06-24 21:55:50 -0500

    feat: update amount value object

--- src/main/java/com/example/MathPlayOpen/performance/domain/model/valueobjects/Amount.java
+++ src/main/java/com/example/MathPlayOpen/performance/domain/model/valueobjects/Amount.java
@@ -4,5 +4,10 @@ package com.example.MathPlayOpen.performance.domain.model.valueobjects;
  * @author Sharon Antuanet Ivet Barrial Marin - u202114900
  * @version 1.0
  */
-public record Amount() {
+public record Amount(Integer amount) {
+    public Amount {
+        if (amount == null || amount < 0) {
+            throw new IllegalArgumentException("Amount must not be null or negative");
+        }
+    }
 }
