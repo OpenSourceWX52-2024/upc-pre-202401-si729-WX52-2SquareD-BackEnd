447001ab82617aefe2337e660ec825d007dc5a07
MiguelC <sackito992@gmail.com>
2024-06-24 20:49:13 -0500

    feat: delete bc

--- src/main/java/com/example/MathPlayOpen/profile/domain/model/commands/CreateProfileCommand.java
+++
@@ -1,4 +0,0 @@
-package com.example.MathPlayOpen.profile.domain.model.commands;
-
-public record CreateProfileCommand(String firstName, String lastName, String email, String password) {
-}
--- src/main/java/com/example/MathPlayOpen/profile/interfaces/rest/resources/ProfileResource.java
+++
@@ -1,4 +0,0 @@
-package com.example.MathPlayOpen.profile.interfaces.rest.resources;
-
-public record ProfileResource(Long id,String fullName, String email, String password) {
-}
