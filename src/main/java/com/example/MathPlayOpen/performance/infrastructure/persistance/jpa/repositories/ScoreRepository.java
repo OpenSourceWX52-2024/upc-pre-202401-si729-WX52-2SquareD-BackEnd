76efe47151cb033049dfd409006013b86d47fd96
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-06-24 22:18:43 -0500

    feat: add repository

---
+++ src/main/java/com/example/MathPlayOpen/performance/infrastructure/persistance/jpa/repositories/ScoreRepository.java
@@ -0,0 +1,15 @@
+package com.example.MathPlayOpen.performance.infrastructure.persistance.jpa.repositories;
+
+import com.example.MathPlayOpen.performance.domain.model.aggregates.Score;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Sharon Antuanet Ivet Barrial Marin - u202114900
+ * @version 1.0
+ */
+
+@Repository
+public interface ScoreRepository extends JpaRepository<Score, Long> {
+    
+}
