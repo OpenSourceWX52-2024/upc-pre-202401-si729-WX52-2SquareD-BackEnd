2dced784753ac58abddac2d839416fd72b5c4ed6
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-06-25 01:51:36 -0500

    fix: IAM 2.0

--- src/main/java/com/example/MathPlayOpen/iam/domain/services/UserQueryService.java
+++ src/main/java/com/example/MathPlayOpen/iam/domain/services/UserQueryService.java
@@ -2,8 +2,8 @@ package com.example.MathPlayOpen.iam.domain.services;
 
 import com.example.MathPlayOpen.iam.domain.model.aggregates.User;
 import com.example.MathPlayOpen.iam.domain.model.queries.GetAllUsersQuery;
+import com.example.MathPlayOpen.iam.domain.model.queries.GetUserByEmailQuery;
 import com.example.MathPlayOpen.iam.domain.model.queries.GetUserByIdQuery;
-import com.example.MathPlayOpen.iam.domain.model.queries.GetUserByUsernameQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +11,6 @@ import java.util.Optional;
 public interface UserQueryService {
     List<User> handle(GetAllUsersQuery query);
     Optional<User> handle(GetUserByIdQuery query);
-    Optional<User> handle(GetUserByUsernameQuery query);
+    Optional<User> handle(GetUserByEmailQuery query);
 
 }
