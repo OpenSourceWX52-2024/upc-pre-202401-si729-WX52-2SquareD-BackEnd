cbd1a32581a83a2c3dfd700dcbf46668478a0ecb
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2024-06-24 22:28:58 -0500

    feat: add resources elements

---
+++ src/main/java/com/example/MathPlayOpen/performance/interfaces/rest/resources/CreateScoreResource.java
@@ -0,0 +1,10 @@
+package com.example.MathPlayOpen.performance.interfaces.rest.resources;
+
+/**
+ * @author Sharon Antuanet Ivet Barrial Marin - u202114900
+ * @version 1.0
+ */
+public record CreateScoreResource(
+
+) {
+}
